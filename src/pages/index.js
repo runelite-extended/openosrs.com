5e84aabb2631c66cde64eefb46ed3781d007ea2a
James Munson <jmunson@openpoll.io>
2019-05-16 17:31:45 -0700

    Fix?

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,9 +28,9 @@ export default class Index extends Component {
 
 
           
-        <div class="col-12 col-sm-6 col-lg-3 pt-4 pt-sm-0"><p> <i style="font-size: 52px; color: Dodgerblue;" class=""></i></p><h3>Plugin Support</h3><p>Load your own plugins with our external jar plugin support. Simple and easy!</p></div>
-        <div class="col-12 col-sm-6 col-lg-3 pt-4 pt-lg-0"><p><i style="font-size: 52px; color: Dodgerblue;" class=""></i></p><h3>Open Source</h3><p>This is meant to directly compete with 3rd party clients that are trying to sell their code.</p></div>
-        <div class="col-12 col-sm-6 col-lg-3 pt-4 pt-lg-0"><p><i style="font-size: 52px; color: Dodgerblue;" class=""></i></p><h3>Quick Updates</h3><p>Updated often! Check the git commits below to see our newest changes.</p></div>
+        <div class="col-12 col-sm-6 col-lg-3 pt-4 pt-sm-0"><h3>Plugin Support</h3><p>Load your own plugins with our external jar plugin support. Simple and easy!</p></div>
+        <div class="col-12 col-sm-6 col-lg-3 pt-4 pt-lg-0"><h3>Open Source</h3><p>This is meant to directly compete with 3rd party clients that are trying to sell their code.</p></div>
+        <div class="col-12 col-sm-6 col-lg-3 pt-4 pt-lg-0"><h3>Quick Updates</h3><p>Updated often! Check the git commits below to see our newest changes.</p></div>
 
           </div>
           </div>
