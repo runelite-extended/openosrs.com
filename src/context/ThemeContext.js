f296cec8d3deb174420a4c3658ab956a71b11416
James Munson <jmunson@openpoll.io>
2019-05-24 16:20:25 -0700

    Remove unused code

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,37 +1,14 @@
 import React, { Component } from 'react'
 
 const defaultState = {
-  dark: false,
   notFound: false,
-  toggleDark: () => {},
 }
 
 const ThemeContext = React.createContext(defaultState)
 
 class ThemeProvider extends Component {
   state = {
-    dark: false,
     notFound: false,
-  }
-
-  componentDidMount() {
-    const lsDark = JSON.parse(localStorage.getItem('darkk'))
-
-    if (lsDark) {
-      this.setState({ dark: lsDark })
-    }
-  }
-
-  componentDidUpdate(prevState) {
-    const { dark } = this.state
-
-    if (prevState.dark !== dark) {
-      localStorage.setItem('darkk', JSON.stringify(dark))
-    }
-  }
-
-  toggleDark = () => {
-    this.setState(prevState => ({ dark: !prevState.dark }))
   }
 
   setNotFound = () => {
@@ -44,16 +21,14 @@ class ThemeProvider extends Component {
 
   render() {
     const { children } = this.props
-    const { dark, notFound } = this.state
+    const { notFound } = this.state
 
     return (
       <ThemeContext.Provider
         value={{
-          dark,
           notFound,
           setFound: this.setFound,
           setNotFound: this.setNotFound,
-          toggleDark: this.toggleDark,
         }}
       >
         {children}
