24eef5082064afc82b9627f51772adf9351669bb
James Munson <jmunson@openpoll.io>
2019-05-24 00:01:53 -0700

    Footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <footer className="footer container">
+        <footer class="footer container" data-block-type="footers" data-id="1">
+          
         <a href="https://twitter.com/runeliteplus" target="_blank" rel="noopener noreferrer">
           Twitter
         </a>
@@ -16,6 +17,12 @@ export default class Footer extends Component {
           rel="noopener noreferrer"
         >
           View source
+        </a>
+        <a href="http://gatsbyjs.org/"  class="footer22" target="_blank" rel="noopener noreferrer">
+        Developed with GatsbyJS<img src="/img/gjs.svg"  class="footer23" alt="RuneLitePlus"></img>
+        </a>
+        <a href="https://runelitepl.us/"  target="_blank" rel="noopener noreferrer">
+        <div class="footer24">RuneLitePlus is not affiliated with Jagex or RuneLite in any way.</div>
         </a>
       </footer>
     )
