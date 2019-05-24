c88d2fa21b7e7b4648c2646dfc4f865cab960a57
James Munson <jmunson@openpoll.io>
2019-05-23 21:06:57 -0700

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default class Index extends Component {
         <section class="test" >
           
           <div class="row justify-content-center">
-          <div class="col-2 col-md-8 col-lg-6 text-center pb-md-2"><p class="lead2"><h1>RuneLitePlus</h1></p><p class="lead">Fork of RuneLite that provides more functionality and less restrictions while staying open source.&nbsp;</p><p class="mt-4"><a class="btn btn-primary" href="https://runelitepl.us/RuneLitePlus.jar">Download</a></p>
+          <div class="col-2 col-md-8 col-lg-6 text-center pb-md-2"><p class="lead2"><h1>RuneLitePlus</h1></p><p class="lead">Fork of RuneLite that provides more functionality and less restrictions while staying open source.</p><p class="mt-4"><a class="btn btn-primary" href="https://runelitepl.us/RuneLitePlus.jar">Download</a></p>
           </div>
 
           </div>
