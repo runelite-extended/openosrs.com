f3b777e12cdbe0bff5d382768af1e7f9c8f89461
James Munson <jmunson@openpoll.io>
2019-05-31 01:13:31 -0700

    Remove imgs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ export default class Footer extends Component {
       <footer class="footer container" data-block-type="footers" data-id="1">
 
         <a href="https://twitter.com/runeliteplus"  class="footermar" target="_blank" rel="noopener noreferrer">
-          Twitter<img src="/img/twitter.png" class="footericon" alt="RuneLitePlus"></img>
+          Twitter
         </a>
         <a href="http://gatsbyjs.org/" class="footermar" target="_blank" rel="noopener noreferrer">
-          Developed with GatsbyJS<img src="/img/gjs.svg" class="footericon" alt="RuneLitePlus"></img>
+          Developed with GatsbyJS
         </a>
         <a href="https://www.netlify.com" class="footermar" target="_blank" rel="noopener noreferrer">
-          Hosted by Netlify<img src="/img/net32.png" class="footericon" alt="netlify"></img>
+          Hosted by Netlify
         </a>
         <a href="https://runelitepl.us/"  target="_blank" rel="noopener noreferrer">
           RuneLite Plus is not affiliated with jagex or runelite in any way.
