459154336f39e63617dd48ebde765cd442761b9d
Owain van Brakel <owain.vanbrakel@gmail.com>
2019-06-08 04:41:04 +0200

    Webp header

--- src/app/components/content/app-home/home.component.ts
+++ src/app/components/content/app-home/home.component.ts
@@ -1,9 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.pug',
   styleUrls: ['./home.component.scss']
 })
-export class AppHomeComponent {
+export class AppHomeComponent implements OnInit {
+
+  public webp: boolean;
+
+  ngOnInit(): void {
+    this.webp = document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') === 0;
+
+    console.log(this.webp);
+  }
 }
