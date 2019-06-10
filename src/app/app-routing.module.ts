f6e010f2bf6e904a7576232090513460793d363e
Owain van Brakel <owain.vanbrakel@gmail.com>
2019-06-10 03:33:29 +0200

    Add back preloading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [
     RouterModule
