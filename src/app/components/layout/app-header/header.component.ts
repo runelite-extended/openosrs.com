7258c8b5871800bb11e9c7b4b4f7c775e2e6ea10
Owain van Brakel <owain.vanbrakel@gmail.com>
2019-06-13 11:46:23 +0200

    Revert "Theme switcher for non apple browsers (Might finish and fix once upon a time)"
    This reverts commit c60da9a96a3c5240de3197e264b880ee9773a51a.

--- src/app/components/layout/app-header/header.component.ts
+++ src/app/components/layout/app-header/header.component.ts
@@ -1,10 +1,6 @@
-import {ChangeDetectionStrategy, Component, Inject, OnInit, Renderer2} from '@angular/core';
-import {DOCUMENT} from '@angular/common';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {DomSanitizer} from '@angular/platform-browser';
 import {MatIconRegistry} from '@angular/material';
-
-import {LocalStorageService} from '../../../services/localstorage.service';
-import {ThemeService} from '../../../services/theme.service';
 
 @Component({
   selector: 'app-header',
@@ -14,57 +10,15 @@ import {ThemeService} from '../../../services/theme.service';
 })
 export class AppHeaderComponent implements OnInit {
 
-  public switcher: boolean;
-  public theme: string;
-
   public constructor(
     private domSanitizer: DomSanitizer,
-    private matIconRegistry: MatIconRegistry,
-    private renderer: Renderer2,
-    private localStorageService: LocalStorageService,
-    @Inject(DOCUMENT) private document: Document,
-    private themeService: ThemeService
-  ) {
-    const style = getComputedStyle(document.documentElement).getPropertyValue('content');
-
-    if (style !== "dark" && style !== "light") {
-      this.switcher = localStorageService.checkSupport();
-    } else if (style === "dark") {
-      getComputedStyle(document.documentElement).setProperty('content', 'dark');
-    } else if (style === "light") {
-    }
-
-    if (this.switcher) {
-      this.localStorageService.getPromise('theme').then((theme) => {
-        if (theme === 'dark') {
-          this.theme = "dark";
-        } else {
-          this.theme = "light";
-        }
-
-        this.themeService.setTheme(this.theme);
-      });
-    }
-  }
+    public matIconRegistry: MatIconRegistry
+  ) {}
 
   ngOnInit(): void {
     this.matIconRegistry.addSvgIcon('twitter', this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/fa/twitter-brands.svg'));
     this.matIconRegistry.addSvgIcon('discord', this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/fa/discord-brands.svg'));
     this.matIconRegistry.addSvgIcon('github', this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/fa/github-brands.svg'));
     this.matIconRegistry.addSvgIcon('patreon', this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/fa/heart-solid.svg'));
-    this.matIconRegistry.addSvgIcon('moon', this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/fa/moon-solid.svg'));
-    this.matIconRegistry.addSvgIcon('sun', this.domSanitizer.bypassSecurityTrustResourceUrl('/assets/fa/sun-solid.svg'));
-  }
-
-  public switchTheme() {
-    this.localStorageService.setPromise('theme', this.theme === 'dark' ? "light" : "dark").then((theme) => {
-      if (theme === 'dark') {
-        this.theme = "dark";
-      } else {
-        this.theme = "light";
-      }
-
-      this.themeService.setTheme(this.theme);
-    });
   }
 }
