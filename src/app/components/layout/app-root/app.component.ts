4b1fee8e31176e2f46bc52e1364a0c9d1478c22d
Owain van Brakel <owain.vanbrakel@gmail.com>
2019-06-13 11:48:46 +0200

    Inject document

--- src/app/components/layout/app-root/app.component.ts
+++ src/app/components/layout/app-root/app.component.ts
@@ -1,9 +1,10 @@
 /// <reference path="../../../../../node_modules/@types/google.analytics/index.d.ts" />
 
-import {Component, OnInit, Renderer2} from '@angular/core';
+import {Component, Inject, OnInit, Renderer2} from '@angular/core';
 import {NavigationEnd, Router} from '@angular/router';
 
 import {UpdateService} from '../../../services/update.service';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,8 @@ export class AppComponent implements OnInit {
   constructor(
     private updateService: UpdateService,
     private router: Router,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
   ) {
     this.style = getComputedStyle(document.documentElement).getPropertyValue('content');
     if (this.style === 'dark') {
