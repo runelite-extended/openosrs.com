c30a7e211228ab0603df9f959c90c856904ab763
Owain van Brakel <owain.vanbrakel@gmail.com>
2019-06-13 11:53:47 +0200

    Native window ref

--- src/app/components/layout/app-root/app.component.ts
+++ src/app/components/layout/app-root/app.component.ts
@@ -1,10 +1,12 @@
 /// <reference path="../../../../../node_modules/@types/google.analytics/index.d.ts" />
 
 import {Component, Inject, OnInit, Renderer2} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 import {NavigationEnd, Router} from '@angular/router';
 
+import {WindowRef} from '../../../window.ref';
+
 import {UpdateService} from '../../../services/update.service';
-import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -19,17 +21,20 @@ export class AppComponent implements OnInit {
     private updateService: UpdateService,
     private router: Router,
     private renderer: Renderer2,
-    @Inject(DOCUMENT) private document: Document
+    @Inject(DOCUMENT) private document: Document,
+    private windowRef: WindowRef
   ) {
     this.style = getComputedStyle(document.documentElement).getPropertyValue('content');
     if (this.style === 'dark') {
       this.renderer.addClass(document.body, "runelite-plus-dark-theme");
     }
 
+    const nativeWindow = this.windowRef.nativeWindow;
+
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        ga('set', 'page', event.urlAfterRedirects);
-        ga('send', 'pageview');
+        nativeWindow.ga('set', 'page', event.urlAfterRedirects);
+        nativeWindow.ga('send', 'pageview');
       }
     });
   }
--- src/app/services/google.analytics.service.ts
+++ src/app/services/google.analytics.service.ts
@@ -2,18 +2,24 @@
 
 import {Injectable} from '@angular/core';
 
+import {WindowRef} from '../window.ref';
+
 @Injectable({
   providedIn: 'root'
 })
 export class GoogleAnalyticsService {
 
-  public static eventEmitter(
+  constructor(
+    private windowRef: WindowRef
+  ) {}
+
+  public eventEmitter(
     eventCategory: string,
     eventAction: string,
     eventLabel: string = null,
     eventValue: number = null
   ) {
-    ga('send', 'event', {
+    this.windowRef.nativeWindow.ga('send', 'event', {
       eventCategory: eventCategory,
       eventLabel: eventLabel,
       eventAction: eventAction,
