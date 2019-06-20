784b6f612755dcd171fcace023113ab08b61c915
Owain van Brakel <owain.vanbrakel@gmail.com>
2019-06-20 15:54:53 +0200

    Download page

--- src/app/components/content/app-downloads/downloads.component.ts
+++ src/app/components/content/app-downloads/downloads.component.ts
@@ -1,21 +1,30 @@
 import { HashReturn } from './../../../interfaces/github.interface';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
 
 import { NotificationService } from '../../../services/notification.service';
 import { GithubService } from 'src/app/services/github.service';
+import { MatStepper } from '@angular/material';
 
 @Component({
   selector: 'app-downloads',
   templateUrl: './downloads.component.html',
-  styleUrls: ['./downloads.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./downloads.component.scss']
 })
 export class AppDownloadsComponent implements OnInit {
 
+  @ViewChild('stepper', { static: false }) stepper: MatStepper;
+
+  public downloadUrl: string;
   public md5hash: HashReturn;
   public fatalMdError = false;
   public sha1hash: HashReturn;
   public fatalShaError = false;
+
+  private clientVersion: string;
+  private selectedDownload: number;
+
+  public hideHashes = true;
+  public selectedOperatingSystem = 'Windows';
 
   constructor(
     private notificationService: NotificationService,
@@ -26,18 +35,62 @@ export class AppDownloadsComponent implements OnInit {
   ngOnInit(): void {
     this.githubService.getLatestClient().then(
       (client) => {
+        this.downloadUrl = client.download;
+        this.clientVersion = client.version;
+
         client.md5
-        .then((data) => this.md5hash = data)
-        .catch(() => this.fatalMdError = true);
+          .then((data) => this.md5hash = data)
+          .catch(() => this.fatalMdError = true);
 
         client.sha1
-        .then((data) => this.sha1hash = data)
-        .catch(() => this.fatalShaError = true);
+          .then((data) => this.sha1hash = data)
+          .catch(() => this.fatalShaError = true);
       }
     ).catch(
       () => {
         console.log('oei');
       }
     );
+  }
+
+  public getHash(item: number): string {
+    if (!this.hideHashes) {
+      if (this.selectedDownload === 0) {
+        if (item === 0) {
+          return 'b8842939dc4e218e15d7bada13d680c8';
+        } else if (item === 1) {
+          return 'fc0cbc5cf3e7ca0f80018e0bdb2aa8fe9ba42426';
+        }
+      } else {
+        if (item === 0) {
+          if (this.fatalMdError || this.clientVersion !== this.md5hash.version) {
+            return 'MD5 hash could not be fetched.';
+          } else {
+            return this.md5hash.hash;
+          }
+        } else if (item === 1) {
+          if (this.fatalShaError || this.clientVersion !== this.sha1hash.version) {
+            return 'SHA1 hash could not be fetched.';
+          } else {
+            return this.sha1hash.hash;
+          }
+        }
+      }
+    }
+  }
+
+  public downloadClick(item: number): void {
+    if (item === 0 && this.selectedOperatingSystem !== 'Windows') {
+      return;
+    }
+
+    this.selectedDownload = item;
+
+    this.stepper.selected.completed = true;
+    this.stepper.next();
+
+    this.hideHashes = false;
+
+    this.notificationService.showError('Starting download, please wait!');
   }
 }
--- src/app/components/content/app-downloads/downloads.module.ts
+++ src/app/components/content/app-downloads/downloads.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule, MatStepperModule, MatFormFieldModule, MatSelectModule, MatCardModule } from '@angular/material';
 
 import { DownloadsRoutingModule } from './downloads.routing';
 
@@ -12,6 +13,10 @@ import { AppDownloadsComponent } from './downloads.component';
     CommonModule,
     HttpClientModule,
     FlexLayoutModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCardModule,
 
     DownloadsRoutingModule,
   ],
